d9a7afa5e3c936e43607a53ce44114adcadc4133
Emerson Barina <emersonbarina@gmail.com>
2018-08-01 22:13:34 -0300

    Mostrando as categorias

--- Documents/workspace-ionic/CursoSpringIonic/src/config/api.config.ts
+++ Documents/workspace-ionic/CursoSpringIonic/src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseurl: "http://localhost:8090"
+    baseurl: "http://localhost:8090", 
+    bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/barina-curso-spring-ionic"
 }
