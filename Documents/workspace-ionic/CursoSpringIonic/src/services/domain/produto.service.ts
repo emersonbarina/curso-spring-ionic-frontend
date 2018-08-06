72f7bd61ac6e33508d0b0f5a33acc01e8cb4c4ca
Emerson Barina <emersonbarina@gmail.com>
2018-08-05 22:30:55 -0300

    Carregando imagens dos produtos

--- Documents/workspace-ionic/CursoSpringIonic/src/services/domain/produto.service.ts
+++ Documents/workspace-ionic/CursoSpringIonic/src/services/domain/produto.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "../../../node_modules/@angular/core";
 import { HttpClient } from "../../../node_modules/@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
+import { Observable } from "rxjs/Rx";
 
 @Injectable()
 export class ProdutoService {
@@ -10,5 +11,10 @@ export class ProdutoService {
 
     findByCategoria(categoria_id : string) {
         return this.http.get(`${API_CONFIG.baseurl}/produtos/?categorias=${categoria_id}`);
+    }
+
+    getSmallImageFromBucket(id : string) : Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
+        return this.http.get(url, {responseType : 'blob'});
     }
 }
