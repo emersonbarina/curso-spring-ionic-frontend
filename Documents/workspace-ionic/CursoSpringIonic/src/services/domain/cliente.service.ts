99d2a9923164c594ac1efcde0a7633c864a242e0
Emerson Barina <emersonbarina@gmail.com>
2018-08-10 20:00:48 -0300

    Fazendo upload da foto

--- Documents/workspace-ionic/CursoSpringIonic/src/services/domain/cliente.service.ts
+++ Documents/workspace-ionic/CursoSpringIonic/src/services/domain/cliente.service.ts
@@ -4,12 +4,16 @@ import { Observable } from "rxjs/Rx";
 import { ClienteDTO } from "../../models/cliente.dto";
 import { API_CONFIG } from "../../config/api.config";
 import { StorageService } from "../storage.service";
+import { ImageUtilService } from "../image-util.service";
 
 
 @Injectable()
 export class ClienteService {
 
-    constructor(public http: HttpClient, public storage: StorageService) {
+    constructor(
+        public http: HttpClient, 
+        public storage: StorageService,
+        public imageUtilService: ImageUtilService) {
     }
 
     findById(id: string) {
@@ -29,6 +33,21 @@ export class ClienteService {
         return this.http.post(
             `${API_CONFIG.baseurl}/clientes`,
             obj,
+            {
+                observe: 'response',
+                responseType: 'text'
+            }
+        );
+    }
+
+    uploadPicture(picture) {
+        // Converter a imagem de Base64 para Blob
+        let pictureBlob = this.imageUtilService.dataUriToBlob(picture);
+        let formData : FormData = new FormData();
+        formData.set('file', pictureBlob, 'file.png');
+        return this.http.post(
+            `${API_CONFIG.baseurl}/clientes/picture`,
+            formData,
             {
                 observe: 'response',
                 responseType: 'text'
