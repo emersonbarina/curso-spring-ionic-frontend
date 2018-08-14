b685571826ba8cffd657856d5019fbe38e14adfd
Emerson Barina <emersonbarina@gmail.com>
2018-08-14 10:56:17 -0300

    Bugfix - atualizacao da imagem de perfil

--- Documents/workspace-ionic/CursoSpringIonic/src/pages/profile/profile.ts
+++ Documents/workspace-ionic/CursoSpringIonic/src/pages/profile/profile.ts
@@ -5,7 +5,8 @@ import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { API_CONFIG } from '../../config/api.config';
 import { CameraOptions, Camera } from '../../../node_modules/@ionic-native/camera';
-
+import { DomSanitizer } from '@angular/platform-browser';
+ 
 @IonicPage()
 @Component({
   selector: 'page-profile',
@@ -16,6 +17,7 @@ export class ProfilePage {
   cliente: ClienteDTO;
 
   picture: string;
+  profileImage;
   cameraOn: boolean = false;
 
   constructor(
@@ -23,7 +25,10 @@ export class ProfilePage {
     public navParams: NavParams,
     public storage: StorageService,
     public clienteService: ClienteService,
-    public camera: Camera) {
+    public camera: Camera,
+    public sanitizer: DomSanitizer) {
+
+      this.profileImage = 'assets/imgs/avata-blank.png';
   }
 
   ionViewDidLoad() {
@@ -53,8 +58,23 @@ export class ProfilePage {
     this.clienteService.getImageFromBucket(this.cliente.id)
     .subscribe(response => {
       this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      this.blobToDataURL(response).then(dataUrl => {
+        let str : string = dataUrl as string;
+        this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+      })
     },
-    error => {});
+    error => {
+      this.profileImage = 'assets/imgs/avata-blank.png';
+    });
+  }
+
+  blobToDataURL(blob) {
+    return new Promise((fulfill, reject) => {
+        let reader = new FileReader();
+        reader.onerror = reject;
+        reader.onload = (e) => fulfill(reader.result);
+        reader.readAsDataURL(blob);
+    })
   }
 
   getCameraPicture() { 
@@ -98,7 +118,7 @@ export class ProfilePage {
     this.clienteService.uploadPicture(this.picture) 
       .subscribe(response => {
         this.picture = null;
-        this.loadData();
+        this.getImageIfExists();
       },
       error => {
       });
