71a0383f5ca7701e6b6e2fe3a12cb35d802b39a8
Emerson Barina <emersonbarina@gmail.com>
2018-08-13 22:42:36 -0300

    Testando o app no dispositivo

--- Documents/workspace-ionic/CursoSpringIonic/src/config/api.config.ts
+++ Documents/workspace-ionic/CursoSpringIonic/src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseurl: "http://localhost:8090", 
+    baseurl: "https://barina-curso-spring-ionic.herokuapp.com", 
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/barina-curso-spring-ionic"
 }
